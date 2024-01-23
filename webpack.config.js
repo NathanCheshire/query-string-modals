2d1d410f28b82b7409dd1b234dfa46eb50cc0c00
Nathan Cheshire <nathan.vincent.2.718@gmail.com>
2024-01-22 20:29:54 -0600

    updating gitignore and cached modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,10 @@
-// webpack.config.js
-
 const path = require("path");
 
 module.exports = {
   entry: "./src/index.ts",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "index.js", // Compiled JavaScript file
+    filename: "index.js",
     libraryTarget: "umd",
     globalObject: "this",
   },
