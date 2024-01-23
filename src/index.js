e297fa6e72aef0f55367eb7ac5eb70fbfa9b74fd
Nathan Cheshire <nathan.vincent.2.718@gmail.com>
2024-01-22 20:04:10 -0600

    updating exports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 export * from "./components/ManagedModal";
 export * from "./components/ModalCallbacks";
-export { default as ModalProvider } from "./components/ModalManager";
+export { default as ModalProvider, useModal } from "./components/ModalManager";
