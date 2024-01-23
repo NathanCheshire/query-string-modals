410c21006237f1ce35ee59981583e44184b53aac
Nathan Cheshire <nathan.vincent.2.718@gmail.com>
2024-01-22 19:52:29 -0600

    working on build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 import { resolve as _resolve } from "path";
 
-export const entry = "./src/index.ts";
+export const entry = "./src/index.js";
 export const output = {
   path: _resolve(__dirname, "dist"),
   filename: "index.ts",
